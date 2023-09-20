c0bcafa2c5f6124a95f25e99aaf58694475b327a
Matthew Gilligan <mgilligan7043@student.pps.net>
2023-09-20 11:42:10 -0700

    fixed layering issue, still working on inventory

--- Assets/GardenScripts/inventory.cs
+++ Assets/GardenScripts/inventory.cs
@@ -16,6 +16,25 @@ public class inventory
             count = 0;
             maxAllowed = 99; 
         }
+
+        public bool canAddItem()
+        {
+            if(count < maxAllowed)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void addItem(CollectableType type)
+        {
+            this.type = type;
+            count++;
+        }
+
     }
 
     public List<Slot> slots = new List<Slot>();
@@ -26,6 +45,19 @@ public class inventory
         {
             Slot slot = new Slot();
             slots.Add(slot);
+        }
+    }
+
+
+    public void Add(CollectableType typeToAdd)
+    {
+        foreach(Slot slot in slots)
+        {
+            if(slot.type == typeToAdd && slot.canAddItem())
+            {
+                slot.addItem(typeToAdd);
+                return;
+            }
         }
     }
 
