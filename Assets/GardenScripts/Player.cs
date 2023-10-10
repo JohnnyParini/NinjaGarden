0dd390a9d6e819d461f8af10672a05a05608895b
Matthew Gilligan <mgilligan7043@student.pps.net>
2023-10-10 11:36:26 -0700

    fixed one inventory error, found two more

--- Assets/GardenScripts/Player.cs
+++ Assets/GardenScripts/Player.cs
@@ -16,15 +16,16 @@ public class Player : MonoBehaviour
 
     public void Update()
     {
-
+        
        //need to get the count variable from inventory and update it. 
+       //never mind, I fixed it
     }
 
     public void DropItem(collectable item)
     {
         Vector3 spawnLocation = transform.position; //the location of the player when the player drops the item
 
-        Vector3 spawnOffset = Random.insideUnitCircle * 1.25f; //so it doesn't spawn on the player, make an offset
+        Vector3 spawnOffset = Random.insideUnitCircle * 10.25f; //so it doesn't spawn on the player, make an offset
 
         collectable droppedItem = Instantiate(item, spawnLocation + spawnOffset, Quaternion.identity); //spawn at location + offset
 
