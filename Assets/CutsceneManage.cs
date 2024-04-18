6c6045e7f15dc45946ef927e73162e64b5af5bb1
Matthew Gilligan <mgilligan7043@student.pps.net>
2024-04-18 12:00:36 -0700

    productive day today. task: re-rig animations, cutscenes

---
+++ Assets/CutsceneManage.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CutsceneManage : MonoBehaviour
+{
+    [SerializeField] GameObject UICanvas;
+    [SerializeField] GameObject cutSceneCanvas;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enableCutscene();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void enableUI()
+    {
+        cutSceneCanvas.SetActive(false);
+        UICanvas.SetActive(true);
+    }
+
+    void enableCutscene()
+    {
+        UICanvas.SetActive(false);
+        cutSceneCanvas.SetActive(true);
+    }
+
+}
